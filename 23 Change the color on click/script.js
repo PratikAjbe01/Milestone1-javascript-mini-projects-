d3c215ff8dd4b00e4f04fe0c0c4faceaa2980941
Pratik Ajbe <124169308+PratikAjbe01@users.noreply.github.com>
2023-03-20 22:43:27 +0530

    Add files via upload

---
+++ 23 Change the color on click/script.js
@@ -0,0 +1,13 @@
+const btn=document.getElementById("button");
+const randomcolor=()=>{
+    let val="0123456789ABCDEF";
+    let cons="#";
+    for(let i=0;i<6;i++){
+        cons=cons+val[Math.floor(Math.random()*16)];
+    }
+    return cons;
+}
+function changerandomcolor(){
+    document.body.style.backgroundColor=randomcolor();
+}
+btn.addEventListener("click",changerandomcolor);        
